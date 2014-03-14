d7cbee930dfe63dd3b727911795d79fcc2f70e15
NosovDE <NosovDE@gmail.com>
2014-03-14 12:52:28 +0400

    First commit

---
+++ src/main/java/ru/nde/MyBenchmark.java
@@ -0,0 +1,12 @@
+package ru.nde;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+
+public class MyBenchmark {
+
+    @GenerateMicroBenchmark
+    public void testMethod() {
+        // place your benchmarked code here
+    }
+
+}
